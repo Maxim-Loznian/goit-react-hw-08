1d095ed03c05b314d1127cee3632581ebb1c2cce
Maxim <loznyan84@gmail.com>
2024-08-02 09:52:43 +0300

    333

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styles from './Contact.module.css';  // Змінити імпорт CSS
+import styles from './Contact.module.css';
 
 const Contact = ({ id, name, number, onDeleteContact }) => (
   <li className={styles.contact}>
