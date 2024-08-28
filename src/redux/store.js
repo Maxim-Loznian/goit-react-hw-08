d2f59b6fa10168706dd518604f86dd7d3474b1b9
Maxim <loznyan84@gmail.com>
2024-08-28 09:45:01 +0300

    Pravki mentora

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,32 +1,13 @@
+// src/redux/store.js
 import { configureStore } from '@reduxjs/toolkit';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
 import contactsReducer from './contactsSlice';
 import filtersReducer from './filtersSlice';
 
-// Конфігурація персистенції
-const persistConfig = {
-  key: 'root',
-  storage,
-  whitelist: ['items'], // Персистити тільки масив контактів
-};
-
-// Обгортання редюсерів у персистенційний редюсер
-const persistedReducer = persistReducer(persistConfig, contactsReducer);
-
 export const store = configureStore({
   reducer: {
-    contacts: persistedReducer,
+    contacts: contactsReducer,
     filters: filtersReducer,
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        // Вимкнути перевірку серіалізації для редюсера контакту
-        ignoredActions: ['persist/PERSIST'],
-        ignoredPaths: ['contacts'],
-      },
-    }),
 });
 
-export const persistor = persistStore(store);
+export default store;
