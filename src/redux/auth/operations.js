cb7e21772f4da76684227716d163fd1043010839
Maxim <loznyan84@gmail.com>
2024-09-04 12:00:28 +0300

    form styles

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -3,31 +3,60 @@ import axios from 'axios';
 
 const BASE_URL = 'https://connections-api.goit.global';
 
-export const register = createAsyncThunk('auth/register', async (userData) => {
-  const response = await axios.post(`${BASE_URL}/users/signup`, userData);
-  return response.data;
-});
-
-export const login = createAsyncThunk('auth/login', async (userData) => {
-  const response = await axios.post(`${BASE_URL}/users/login`, userData);
-  return response.data;
-});
-
-export const logOut = createAsyncThunk('auth/logout', async () => {
-  await axios.post(`${BASE_URL}/users/logout`);
-});
-
-export const fetchCurrentUser = createAsyncThunk('auth/fetchCurrentUser', async (_, thunkAPI) => {
-  const state = thunkAPI.getState();
-  const token = state.auth.token;
-  if (!token) {
-    return thunkAPI.rejectWithValue('No token found');
+export const register = createAsyncThunk(
+  'auth/register',
+  async (userData, thunkAPI) => {
+    try {
+      const response = await axios.post(`${BASE_URL}/users/signup`, userData);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data.message);
+    }
   }
-  
-  const response = await axios.get(`${BASE_URL}/users/current`, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
-  return response.data;
-});
+);
+
+export const login = createAsyncThunk(
+  'auth/login',
+  async (userData, thunkAPI) => {
+    try {
+      const response = await axios.post(`${BASE_URL}/users/login`, userData);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data.message);
+    }
+  }
+);
+
+export const logOut = createAsyncThunk(
+  'auth/logout',
+  async (_, thunkAPI) => {
+    try {
+      await axios.post(`${BASE_URL}/users/logout`);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data.message);
+    }
+  }
+);
+
+export const fetchCurrentUser = createAsyncThunk(
+  'auth/fetchCurrentUser',
+  async (_, thunkAPI) => {
+    const state = thunkAPI.getState();
+    const token = state.auth.token;
+
+    if (!token) {
+      return thunkAPI.rejectWithValue('No token found');
+    }
+
+    try {
+      const response = await axios.get(`${BASE_URL}/users/current`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data.message);
+    }
+  }
+);
